c38531ce973788cb92936619db38cb0b9bd809be
DavidFFDAW <davidferflo2@gmail.com>
2021-01-07 13:16:31 +0100

    feat(services/cars.service): adding the update methods to the service

--- frontend/src/services/car-renting.cars.service.js
+++ frontend/src/services/car-renting.cars.service.js
@@ -4,6 +4,13 @@ class CarsLocalService{
     }
 
     addCar = newCar => this.cars = [...this.cars,newCar];
+    updateCar = ({registration,brand,model,color}) => {
+        const carToBeUpdated = this.findCarByRegistration(registration);
+        console.log('car-update: ',carToBeUpdated);
+        carToBeUpdated.brand = brand;
+        carToBeUpdated.model = model;
+        carToBeUpdated.color = color;
+    }
     findCarByRegistration = registration => this.cars.find(element => element.registration === registration);
     alreadyExistCarByRegistration = registration => this.cars.filter(element => element.registration === registration).length > 0;
     removeFromArrayByRegistration = registration => this.cars = this.cars.filter(element => element.registration !== registration);
@@ -14,5 +21,12 @@ class CarsLocalService{
         }
         const newCar = new Car(registration,brand,model,color,garage);
         this.addCar(newCar);
+    }
+
+    updateCurrentCar = car => {
+        if(!this.alreadyExistCarByRegistration(car.registration)){
+            throw new Error('No existe el coche que se pretende modificar');
+        }
+        this.updateCar(car);
     }
 }
